a07eebac0613d2e6e8d225f46042597e8f0048e0
Lázaro Alves <lazaroalves@MacBook-Pro-de-Lazaro.local>
2022-07-10 16:52:26 -0300

    Add styles in the component App

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import kemetecLogo from "./imagenes/kemetec-logo.png"; 
 
 function App() {
   return (
@@ -6,9 +7,12 @@ function App() {
       <div className="kemetec-logo-contenedor">
         <img 
           className="kemetec-logo" 
-          src={}
+          src={kemetecLogo}
           alt="Logo de Kemetec" 
         />
+      </div>
+      <div className="contenedor-principal">
+        
       </div>
     </div>
   );
